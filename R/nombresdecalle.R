5ba50c398283f437f86a96dfc5dc32bbf13c630d
Andrés <aeduhour@yahoo.com.ar>
2021-07-17 10:05:57 -0300

    actualizo el listado de paquetes necesarios y descarga del límite provincial

--- R/nombresdecalle.R
+++ R/nombresdecalle.R
@@ -4,14 +4,8 @@
 
 library(dplyr)
 library(tidyr)
-library(ggplot2)
 library(sf)
 library(osmdata)
-library(units)
-library(mapview)
-library(ggmap)
-library(ggspatial)
-library(tmap)
 library(magrittr)
 
 ## Obtener el polígono del área a consultar
@@ -26,22 +20,34 @@ calles <- opq(bbox = recuadro) %>%
                           value = "residential") %>%
   osmdata_sf()
 
+# Seleccionar el límite provincial
+# Obtiene los límites de todas las provincia que limitan con la de Bs As 
+# incluyendo parte de Uruguay
+
+limitebsas <- opq(bbox = recuadro) %>%
+              add_osm_feature(key = "admin_level", value = "4") %>%
+  osmdata_sf()
 
 ## Seleccionar las columnas del nombre y el id de OSM del objeto
 calles <- calles$osm_lines %>%
           select(name,osm_id)
 
+limitebsas_linea <- limitebsas$osm_lines %>%
+                    select(osm_id)
+
 # Establecer el sistema de referencia (aseguramos que todos los 'simple
 # features' tienen el mismo)
 
 st_crs(calles) <- 4326
+st_crs(limitebsas_linea) <- 4326
 
-# Cortar los objetos descargados.
-
-calles <- st_intersection(calles, poligono)
 
 # Grafico de las calles. Lleva tiempo para la provincia de Buenos Aires
 plot(calles) #lerdo
+
+# Gráfico del límite provincial
+
+plot(limitebsas_linea)
 
 # Crea un vector con los nombres de calle existentes  
 nombres <- calles$name
